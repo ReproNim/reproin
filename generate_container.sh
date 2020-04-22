e7ba468f02a45567593e0d0b12dd2b061c947e93
Yaroslav Halchenko <debian@onerussian.com>
2020-04-21 22:53:19 -0400

    RF: move to use all python3-pkgs, and simplify entrypoint

--- generate_container.sh
+++ generate_container.sh
@@ -7,31 +7,34 @@ dev_build=
 
 generate() {
 	if [ "$dev_build" = "1" ]; then
-		apt_pkgs=python-pip
+		apt_pkgs=python3-pip
 		run_cmd="pip install git+https://github.com/nipy/heudiconv@master"
 	else
-		apt_pkgs="heudiconv=0.8.0-1"
+		apt_pkgs="heudiconv=0.8.0-2~nd100+1"
 		run_cmd=":"
 	fi
+	# more details might come on https://github.com/ReproNim/neurodocker/issues/330
+	[ "$1" == singularity ] && add_entry=' "$@"' || add_entry=''
 	#neurodocker generate "$1" \
-	docker run --rm kaczmarj/neurodocker:master generate "$1" \
+	ndversion=0.7.0
+	#ndversion=master
+	docker run --rm repronim/neurodocker:$ndversion generate "$1" \
 		--base=neurodebian:buster \
+	    --ndfreeze date=20200421 \
 		--pkg-manager=apt \
-		--ndfreeze date=20200420T144554Z \
 		--install vim wget strace time ncdu gnupg curl procps datalad pigz less tree \
-				  git-annex-standalone python-nipype virtualenv shellcheck \
-				  python-dcmstack python-configparser python-funcsigs \
-				  python-pytest dcmtk python-pip python-wheel \
-				  python-setuptools python-datalad python-filelock \
-				  dcm2niix python-pytest python3-pytest python3-nose $apt_pkgs \
+				  git-annex-standalone python3-nipype virtualenv shellcheck \
+				  python3-dcmstack python3-funcsigs python3-etelemetry \
+				  python3-pytest dcmtk python3-pip python3-wheel \
+				  python3-setuptools python3-datalad python3-filelock \
+				  dcm2niix python3-pytest python3-nose $apt_pkgs \
 		--run "$run_cmd" \
-		--run "curl -sL https://deb.nodesource.com/setup_6.x | bash - " \
+		--run "curl -sL https://deb.nodesource.com/setup_9.x | bash - " \
 		--install nodejs npm \
 		--run "npm install -g bids-validator@1.5.1" \
 		--run "mkdir /afs /inbox" \
-		--run "echo '#!/bin/bash' >> /neurodocker/heudiconv.sh && echo 'set -eu; heudiconv \"\$@\"' >> /neurodocker/heudiconv.sh && chmod +x /neurodocker/heudiconv.sh" \
 		--user=reproin \
-		--entrypoint '/neurodocker/heudiconv.sh "$@"'
+		--entrypoint "/usr/bin/heudiconv$add_entry"
 }
 
 version=$(git describe)
