6b78056ac190105553d6d869c0dbd967db530304
Yaroslav Halchenko <debian@onerussian.com>
2023-06-05 18:47:07 -0400

    do not bother with cubids and pynidm for now via pip
    Debian is now taking precautiouns so for now let's not bother, might add them
    later or as separate containers
    
    	Step 14/21 : RUN pip install cubids==1.0.8 pynidm==4.0.0
    	 ---> Running in ad3231e05684
    	error: externally-managed-environment
    
    	× This environment is externally managed
    	╰─> To install Python packages system-wide, try apt install
    		python3-xyz, where xyz is the package you are trying to
    		install.
    
    		If you wish to install a non-Debian-packaged Python package,
    		create a virtual environment using python3 -m venv path/to/venv.
    		Then use path/to/venv/bin/python and path/to/venv/bin/pip. Make
    		sure you have python3-full installed.
    
    		If you wish to install a non-Debian packaged Python application,
    		it may be easiest to use pipx install xyz, which will manage a
    		virtual environment for you. Make sure you have pipx installed.
    
    		See /usr/share/doc/python3.11/README.venv for more information.
    
    	note: If you believe this is a mistake, please contact your Python installation or OS distribution provider. You can override this, at the risk of breaking your Python installation or OS, by passing --break-system-packages.
    	hint: See PEP 668 for the detailed specification.

--- generate_container.sh
+++ generate_container.sh
@@ -34,7 +34,6 @@ generate() {
 		--run "curl -sL https://deb.nodesource.com/setup_16.x | bash - " \
 		--install nodejs npm \
 		--run "npm install -g bids-validator@1.11.0" \
-		--run "pip install cubids==1.0.8 pynidm==4.0.0" \
 		--run "mkdir /afs /inbox" \
 		--copy bin/reproin /usr/local/bin/reproin \
 		--run "chmod a+rx /usr/local/bin/reproin" \
